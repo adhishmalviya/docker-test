a6aedfc16eb412dd4bb7a7f028d6fede891daf1a
Your Name <you@example.com>
2020-12-28 12:47:38 +0530

    a

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // app.use(express.urlencoded({ extended: false }));
 
-// Connect to MongoDB
+Connect to MongoDB
 // mongoose
 //   .connect(
 //     'mongodb://mongo:27017/docker-node-mongo',
