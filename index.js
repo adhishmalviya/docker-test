7e24373ec41ce209142b8d514765e12d25461edf
Your Name <you@example.com>
2020-12-28 12:11:44 +0530

    a

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
-mongoose
-  .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+// mongoose
+//   .connect(
+//     'mongodb://mongo:27017/docker-node-mongo',
+//     { useNewUrlParser: true }
+//   )
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err));
 
-const Item = require('./models/Item');
+// const Item = require('./models/Item');
 
 app.get('/', (req, res) => {
-  Item.find()
-    .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
+  res.send("HELLO!")
+  // Item.find()
+  //   .then(items => res.render('index', { items }))
+  //   .catch(err => res.status(404).json({ msg: 'No items found' }));
 });
 
-app.post('/item/add', (req, res) => {
-  const newItem = new Item({
-    name: req.body.name
-  });
+// app.post('/item/add', (req, res) => {
+//   const newItem = new Item({
+//     name: req.body.name
+//   });
 
-  newItem.save().then(item => res.redirect('/'));
-});
+//   newItem.save().then(item => res.redirect('/'));
+// });
 
 const port = 3000;
 
